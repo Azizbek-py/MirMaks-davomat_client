7ce439797268f24a544cf10c58bff92a977dd700
Azizbek-py <azizbektolliboyev155@gmail.com>
2026-05-13 18:40:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -40,8 +40,7 @@ const isLocalhost =
 const API_BASE_URL = isLocalhost
   ? 'http://127.0.0.1:8000'
   : (
-      import.meta.env.VITE_API_URL ||
-      'https://your-render-url.onrender.com'
+      'https://mirmaks-davomat-server.onrender.com'
     );
 
 console.log('API_BASE_URL:', API_BASE_URL);
